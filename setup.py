67d579d8a6efad994924a980e992c1a62534d356
irdb <irdb@users.noreply.github.com>
2015-03-28 08:41:57 +0430

    Adding setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='WikiTextParser',
+    version='0.1.1',
+    description ='A simple, purely python, WikiText parsing tool.',
+    long_description=open('README').read(),
+    author='Dalba',
+    author_email='dalba.wiki@gmail.com',
+    url='https://github.com/irdb/wikitextparser',
+    packages=['wikitextparser'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Text Processing',
+        'Topic :: Utilities',
+    ],
+    license='GNU General Public License v3 (GPLv3)',
+)
+
