1871e30ef7028d5f6341c17ee4d0485463f4fb9b
irdb <irdb@users.noreply.github.com>
2015-03-31 21:06:48 +0430

    version='0.1.3dev',

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='wikitextparser',
-    version='0.1.2',
+    version='0.1.3dev',
     description ='A simple, purely python, WikiText parsing tool.',
     long_description=open('README.rst').read(),
     author='Dalba',
