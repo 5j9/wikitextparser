1e959198eae7eac1b6f3f2d8368230648c1df54e
5j9 <5j9@users.noreply.github.com>
2015-10-20 16:30:16 +0330

    import Table

--- wikitextparser/__init__.py
+++ wikitextparser/__init__.py
@@ -1,4 +1,4 @@
 from .wikitextparser import (
     WikiText, Template, WikiLink, ExternalLink, Section, Argument,
-    Comment, ParserFunction, Parameter, parse
+    Comment, ParserFunction, Parameter, Table, parse
 )
