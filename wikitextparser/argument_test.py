8a551d2abfb595d3441c1899dce543a7ddc06fcd
5j9 <5j9@users.noreply.github.com>
2015-10-21 16:44:37 +0330

    When setting the name, check if the argument is positional

--- wikitextparser/argument_test.py
+++ wikitextparser/argument_test.py
@@ -27,6 +27,17 @@ class Argument(unittest.TestCase):
         a.name = ' c '
         self.assertEqual('| c = b ', a.string)
 
+    def test_set_name_at_subspan_boundary(self):
+        a = wtp.Argument('|{{ a }}={{ b }}')
+        a.name = ' c '
+        self.assertEqual('| c ={{ b }}', a.string)
+        self.assertEqual('{{ b }}', a.value)
+
+    def test_set_name_for_positional_args(self):
+        a = wtp.Argument('| b ')
+        a.name = a.name
+        self.assertEqual('|1= b ', a.string)
+
     def test_set_value(self):
         a = wtp.Argument('| a = b ')
         a.value = ' c '
@@ -43,7 +54,18 @@ class Argument(unittest.TestCase):
         self.assertEqual(True, a.positional)
         self.assertEqual('1', a.name)
         self.assertEqual('<nowiki>1=3</nowiki>', a.value)
-        
 
+    def test_value_after_convertion_of_positional_to_keywordk(self):
+        a = wtp.Argument("""|{{{a|{{{b}}}}}}""")
+        a.name = ' 1 '
+        self.assertEqual('{{{a|{{{b}}}}}}', a.value)
+
+    def test_name_of_positionals(self):
+        self.assertEqual(
+            ['1', '2', '3'],
+            [a.name for a in wtp.parse('{{t|a|b|c}}').templates[0].arguments],
+        )
+
+        
 if __name__ == '__main__':
     unittest.main()
