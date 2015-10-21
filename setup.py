99f42d2db26dc1de75dc864009b32a28199a5fe8
5j9 <5j9@users.noreply.github.com>
2015-10-21 18:09:50 +0330

    version='0.6.2dev',

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='wikitextparser',
-    version='0.6.1',
+    version='0.6.2dev',
     description ='A simple, purely python, WikiText parsing tool.',
     long_description=open('README.rst').read(),
     author='Dalba',
