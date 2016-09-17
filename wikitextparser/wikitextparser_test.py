cd1d2ec0be460caa92dfefa93accd448e941466f
5j9 <5j9@users.noreply.github.com>
2016-09-17 15:13:55 +0430

    Remove the Tag class as it already exists in tag_test.py

--- wikitextparser/wikitextparser_test.py
+++ wikitextparser/wikitextparser_test.py
@@ -3,17 +3,6 @@
 
 import unittest
 
-import wikitextparser as wtp
-
-
-class Tag(unittest.TestCase):
-
-    """Test the Tag class."""
-
-    @unittest.expectedFailure
-    def test_basic(self):
-        t = wtp.Tag('<ref>text</ref>')
-
 
 if __name__ == '__main__':
     tests = unittest.defaultTestLoader.discover('.', '*_test.py')
