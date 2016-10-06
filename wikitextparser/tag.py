bfe8f28503e2b207c9985ce402b42505f2db0d87
5j9 <5j9@users.noreply.github.com>
2016-10-06 13:15:56 +0330

    Use the regex module, some initial changes (this module is not ready yet)

--- wikitextparser/tag.py
+++ wikitextparser/tag.py
@@ -1,51 +1,113 @@
 ﻿"""Define the Tag class."""
 
 
+import regex
+
+from .wikitext import IndexedWikiText
+
+
+regex.DEFAULT_VERSION = regex.VERSION1
 # HTML elements all have names that only use alphanumeric ASCII characters
 # https://www.w3.org/TR/html5/syntax.html#syntax-tag-name
-TAGNAME = r'(?P<tagname>[A-Za-z0-9]+)'
+TAG_NAME = r'(?P<name>[A-Za-z0-9]+)'
 # https://www.w3.org/TR/html5/infrastructure.html#space-character
-SPACE_CHARACTERS = r' \t\n\u000C\r'
+SPACE_CHARS = r' \t\n\u000C\r'
 # http://stackoverflow.com/a/93029/2705757
 # chrs = (chr(i) for i in range(sys.maxunicode))
 # control_chars = ''.join(c for c in chrs if unicodedata.category(c) == 'Cc')
 CONTROL_CHARACTERS = r'\x00-\x1f\x7f-\x9f'
 # https://www.w3.org/TR/html5/syntax.html#syntax-attributes
-ATTRNAME = (
-    r'(?P<attrname>[^' + SPACE_CHARACTERS + CONTROL_CHARACTERS +
-    r'\u0000"\'>/=])'
-)
+ATTR_NAME = (
+    r'(?P<attr_name>[^{SPACE_CHARS}{CONTROL_CHARACTERS}\u0000"\'>/=]+)'
+).format(**locals())
+WS_EQ_WS = r'[{SPACE_CHARS}]*=[{SPACE_CHARS}]*'.format(**locals())
+UNQUOTED_ATTR_VAL = (
+    r'(?P<uq_attr_val>[^{SPACE_CHARS}"\'=<>`]+)'
+).format(**locals())
+QUOTED_ATTR_VAL = r'(?P<quote>[\'"])(?P<q_attr_val>.+?)(?P=quote)'
+# May include character references, but for now, ignore the fact that they
+# cannot contain an ambiguous ampersand.
+ATTR_VAL = (
+    r'''
+    (?:
+        # If an empty attribute is to be followed by the optional
+        # "/" character, then there must be a space character separating
+        # the two. This rule is ignored here.
+        (?P<empty_attr>)[{SPACE_CHARS}]*|
+        {WS_EQ_WS}{UNQUOTED_ATTR_VAL}[{SPACE_CHARS}]*|
+        {WS_EQ_WS}{QUOTED_ATTR_VAL}[{SPACE_CHARS}]*
+    )
+    '''
+).format(**locals())
 # Ignore ambiguous ampersand for the sake of simplicity.
-UNQUOTED_ATTRIBUTE_VALUE = r'([' + SPACE_CHARACTERS + ']*)'
-ATTR = r'(?P<attr> +)'
-STARTTAG = r'(?P<start><' + TAGNAME + r')'
+ATTR = r'(?P<attr>{ATTR_NAME}{ATTR_VAL})'.format(**locals())
+# VOID_ELEMENTS = (
+#     'area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'keygen',
+#     'link', 'meta', 'param', 'source', 'track', 'wbr'
+# )
+# RAW_TEXT_ELEMENTS = ('script', 'style')
+# ESCAPABLE_RAW_TEXT_ELEMENTS = ('textarea', 'title')
+# Detecting foreign elements in MathML and SVG namespaces is not implemented
+# yet. See
+# https://developer.mozilla.org/en/docs/Web/SVG/Namespaces_Crash_Course
+# for an overview.
+START_TAG = (
+    r'''
+    (?P<start>
+        <{TAG_NAME}(?:[{SPACE_CHARS}]+{ATTR})*
+        [{SPACE_CHARS}]*
+        (?:(?P<self_closing>/>)|>)
+    )
+    '''
+).format(**locals())
+START_TAG_REGEX = regex.compile(START_TAG, regex.VERBOSE)
+END_OF_START_TAG = r'(?P<end></(?P=name)[{SPACE_CHARS}]*>)'.format(**locals())
+END_TAG = r'(?P<end></{TAG_NAME}[{SPACE_CHARS}]*>)'.format(**locals())
+END_TAG_REGEX = regex.compile(END_TAG)
+TAG_REGEX = regex.compile(
+    r'''
+    (?P<start>
+        <{TAG_NAME}(?:[{SPACE_CHARS}]+{ATTR})*
+    )
+    # After the attributes, or after the tag name if there are no attributes,
+    # there may be one or more space characters. This is sometimes required but
+    # ignored here.
+    [{SPACE_CHARS}]*
+    (?:
+        (?P<self_closing>/>)|
+        >(?P<contents>.*?){END_OF_START_TAG}|
+        (?P<start_only>>)
+    )
+    '''.format(**locals()),
+    flags=regex.DOTALL | regex.VERBOSE
+)
 
-ATTRIBUTE_VALUES =
 
-import re
-re.sub()
+class Tag(IndexedWikiText):
 
-class ExternalLink():
-
-    """Create a new ExternalLink object."""
+    """Create a new Tag object."""
 
     def __init__(self, string, spans=None, index=None):
-        """Run self._common_init. Set self._spans['extlinks'] if spans is None."""
+        """Initialize the Tag object.
+
+        Run self._common_init. Set self._spans['extlinks'] if spans is None.
+
+        """
         self._common_init(string, spans)
         if spans is None:
-            self._spans['extlinks'] = [(0, len(string))]
+            self._spans['tags'] = [(0, len(string))]
         if index is None:
-            self._index = len(self._spans['extlinks']) - 1
+            self._index = len(self._spans['tags']) - 1
         else:
             self._index = index
 
     def __repr__(self):
-        """Return the string representation of the ExternalLink."""
-        return 'ExternalLink(' + repr(self.string) + ')'
+        """Return the string representation of self."""
+        return 'Tag(' + repr(self.string) + ')'
 
     def _get_span(self):
-        """Return the self-span."""
-        return self._spans['extlinks'][self._index]
+        """Return the span of this object."""
+        return self._spans['tags'][self._index]
 
     @property
     def url(self):
@@ -63,37 +125,6 @@ class ExternalLink():
         else:
             url = self.url
             self.replace_slice(0, len(url), newurl)
-
-    @property
-    def text(self):
-        """Return the display text of the external link.
-
-        Return self.string if this is a bare link.
-        Return
-        """
-        if self.in_brackets:
-            return self.string[1:-1].partition(' ')[2]
-        return self.string
-
-    @text.setter
-    def text(self, newtext):
-        """Set a new text for the current ExternalLink.
-
-        Automatically puts the ExternalLink in brackets if it's not already.
-        """
-        if not self.in_brackets:
-            url = self.string
-            self.strins(len(url), ' ]')
-            self.strins(0, '[')
-            text = ''
-        else:
-            url = self.url
-            text = self.text
-        self.replace_slice(
-            len('[' + url + ' '),
-            len('[' + url + ' ' + text),
-            newtext,
-        )
 
     @property
     def in_brackets(self):
