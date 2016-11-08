c7c75606e5efd2cf9e0fce88fc4556b35740ef09
5j9 <5j9@users.noreply.github.com>
2016-11-08 12:26:32 +0330

    Fix the first test and enable it

--- wikitextparser/cell_test.py
+++ wikitextparser/cell_test.py
@@ -11,11 +11,10 @@ class TableCell(unittest.TestCase):
 
     """Test the Cell class."""
 
-    @unittest.expectedFailure
     def test_basic(self):
         c = Cell('\n| a ')
         self.assertEqual(' a ', c.value)
-        self.assertEqual(repr(c), '\n| a ')
+        self.assertEqual(repr(c), 'Cell(\'\\n| a \')')
 
 
 if __name__ == '__main__':
