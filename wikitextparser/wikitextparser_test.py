761ce2eee9e87e204079075f21a1b236c41187d3
5j9 <5j9@users.noreply.github.com>
2017-02-13 19:13:33 +0330

    Add verbosity=1 to TextTestRunner

--- wikitextparser/wikitextparser_test.py
+++ wikitextparser/wikitextparser_test.py
@@ -7,6 +7,8 @@ import unittest
 
 if __name__ == '__main__':
     test_suite = unittest.defaultTestLoader.discover('.', '*_test.py')
-    test_runner = unittest.TextTestRunner(resultclass=unittest.TextTestResult)
+    test_runner = unittest.TextTestRunner(
+        resultclass=unittest.TextTestResult, verbosity=1
+    )
     result = test_runner.run(test_suite)
     sys.exit(not result.wasSuccessful())
