8e0a842dad2d44f9f8a82658dd1a49afadc166d8
5j9 <5j9@users.noreply.github.com>
2017-06-04 12:46:52 +0430

    from wikitextparser import ExternalLink

--- wikitextparser/externallink_test.py
+++ wikitextparser/externallink_test.py
@@ -3,14 +3,14 @@
 
 import unittest
 
-import wikitextparser as wtp
+from wikitextparser import ExternalLink
 
 
-class ExternalLink(unittest.TestCase):
+class TestExternalLink(unittest.TestCase):
     """Test capturing of external links."""
 
     def test_repr(self):
-        el = wtp.ExternalLink('HTTP://mediawiki.org')
+        el = ExternalLink('HTTP://mediawiki.org')
         self.assertEqual(repr(el), "ExternalLink('HTTP://mediawiki.org')")
 
     def test_numberedmailto(self):
@@ -19,39 +19,39 @@ class ExternalLink(unittest.TestCase):
             'info@example.org?Subject=URL%20Encoded%20Subject&body='
             'Body%20Textinfo]'
         )
-        el = wtp.ExternalLink(s)
+        el = ExternalLink(s)
         self.assertEqual(s[1:-1], el.url)
         self.assertEqual('', el.text)
         self.assertEqual(True, el.in_brackets)
 
     def test_bare_link(self):
-        el = wtp.ExternalLink('HTTP://mediawiki.org')
+        el = ExternalLink('HTTP://mediawiki.org')
         self.assertEqual('HTTP://mediawiki.org', el.url)
         self.assertEqual('HTTP://mediawiki.org', el.text)
         self.assertEqual(False, el.in_brackets)
 
     def test_inbracket_with_text(self):
-        el = wtp.ExternalLink('[ftp://mediawiki.org mediawiki ftp]')
+        el = ExternalLink('[ftp://mediawiki.org mediawiki ftp]')
         self.assertEqual('ftp://mediawiki.org', el.url)
         self.assertEqual('mediawiki ftp', el.text)
         self.assertEqual(True, el.in_brackets)
 
     def test_set_text(self):
-        el = wtp.ExternalLink('[ftp://mediawiki.org mediawiki ftp]')
+        el = ExternalLink('[ftp://mediawiki.org mediawiki ftp]')
         el.text = 'mwftp'
         self.assertEqual('[ftp://mediawiki.org mwftp]', el.string)
-        el = wtp.ExternalLink('ftp://mediawiki.org')
+        el = ExternalLink('ftp://mediawiki.org')
         el.text = 'mwftp'
         self.assertEqual('[ftp://mediawiki.org mwftp]', el.string)
 
     def test_set_url(self):
-        el = wtp.ExternalLink('[ftp://mediawiki.org mw]')
+        el = ExternalLink('[ftp://mediawiki.org mw]')
         el.url = 'https://www.mediawiki.org/'
         self.assertEqual('[https://www.mediawiki.org/ mw]', el.string)
-        el = wtp.ExternalLink('ftp://mediawiki.org')
+        el = ExternalLink('ftp://mediawiki.org')
         el.url = 'https://www.mediawiki.org/'
         self.assertEqual('https://www.mediawiki.org/', el.string)
-        el = wtp.ExternalLink('[ftp://mediawiki.org]')
+        el = ExternalLink('[ftp://mediawiki.org]')
         el.url = 'https://www.mediawiki.org/'
         self.assertEqual('[https://www.mediawiki.org/]', el.string)
 
