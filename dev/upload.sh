7d3e7e51b47a1cef64c5357892c4bd525f273f95
5j9 <5j9@users.noreply.github.com>
2018-01-30 15:51:57 +0330

    mv twine_upload_dist.sh upload.sh

--- dev/upload.sh
+++ dev/upload.sh
@@ -1,12 +1,9 @@
+#!/usr/bin/env bash
 cd ..
 
-echo Creating a Source Distribution:
 python setup.py sdist
-echo Creating a Pure Python Wheel:
 python setup.py bdist_wheel
-
 twine upload dist/*
-
 rm -r dist build wikitextparser.egg-info
 
 read -n1 -r -p 'Done!' key
