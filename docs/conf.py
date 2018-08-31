8d8d3cb139b690456992f8d535b039855b4ecc4d
5j9 <5j9@users.noreply.github.com>
2018-08-31 13:17:14 +0430

    Remove comment that was being detected as a todo item

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
-# If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
 
