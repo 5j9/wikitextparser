edf522ad0bcdb3f882d2efda48fb0c4f735f7c61
5j9 <5j9@users.noreply.github.com>
2019-03-19 21:29:01 +0330

    Template.arguments: No need to call arg_spans.sort()
    insort is used for insertion into arg_spans.

--- wikitextparser/_parser_function.py
+++ wikitextparser/_parser_function.py
@@ -27,30 +27,30 @@ class TlPfMixin(SubWikiText):
     @property
     def arguments(self) -> List[Argument]:
         """Parse template content. Create self.name and self.arguments."""
-        arguments = []
         shadow = self._shadow
         split_spans = self._args_matcher(shadow).spans('arg')
-        if split_spans:
-            arguments_append = arguments.append
-            type_to_spans = self._type_to_spans
-            ss, se = span = self._span
-            type_ = id(span)
-            lststr = self._lststr
-            string = lststr[0]
-            arg_spans = type_to_spans.setdefault(type_, [])
-            span_tuple_to_span_get = {(s[0], s[1]): s for s in arg_spans}.get
-            for arg_self_start, arg_self_end in split_spans:
-                s, e = arg_span = [ss + arg_self_start, ss + arg_self_end]
-                old_span = span_tuple_to_span_get((s, e))
-                if old_span is None:
-                    insort(arg_spans, arg_span)
-                else:
-                    arg_span = old_span
-                arg = Argument(lststr, type_to_spans, arg_span, type_)
-                arg._shadow_cache = (
-                    string[s:e], shadow[arg_self_start:arg_self_end])
-                arguments_append(arg)
-            arg_spans.sort()
+        if not split_spans:
+            return []
+        arguments = []
+        arguments_append = arguments.append
+        type_to_spans = self._type_to_spans
+        ss, se = span = self._span
+        type_ = id(span)
+        lststr = self._lststr
+        string = lststr[0]
+        arg_spans = type_to_spans.setdefault(type_, [])
+        span_tuple_to_span_get = {(s[0], s[1]): s for s in arg_spans}.get
+        for arg_self_start, arg_self_end in split_spans:
+            s, e = arg_span = [ss + arg_self_start, ss + arg_self_end]
+            old_span = span_tuple_to_span_get((s, e))
+            if old_span is None:
+                insort(arg_spans, arg_span)
+            else:
+                arg_span = old_span
+            arg = Argument(lststr, type_to_spans, arg_span, type_)
+            arg._shadow_cache = (
+                string[s:e], shadow[arg_self_start:arg_self_end])
+            arguments_append(arg)
         return arguments
 
 
