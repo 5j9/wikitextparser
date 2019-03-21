c492865e37b60a6ef2a111ff64147d69c652968b
5j9 <5j9@users.noreply.github.com>
2019-03-21 18:55:13 +0330

    tests/__main__.py: Raise an error on warnings

--- tests/__main__.py
+++ tests/__main__.py
@@ -2,8 +2,11 @@
 
 from os import getcwd
 from unittest import defaultTestLoader, TextTestResult, TextTestRunner
+from warnings import resetwarnings, simplefilter
 
 
+resetwarnings()
+simplefilter('error')
 test_suite = defaultTestLoader.discover(
     '.' if getcwd().endswith('tests') else 'tests')
 test_runner = TextTestRunner(resultclass=TextTestResult, verbosity=1)
