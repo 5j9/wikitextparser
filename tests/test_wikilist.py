0c902229d2dc3c47ba04136db3d7045376617ceb
5j9 <5j9@users.noreply.github.com>
2020-04-15 19:44:42 +0430

    refactor(test_wikilist.py)

--- tests/test_wikilist.py
+++ tests/test_wikilist.py
@@ -8,6 +8,7 @@ class WikiListTest(TestCase):
 
     def test_subitem_are_part_of_item(self):
         """A few basic examples from [[mw:Help:Lists]]."""
+        ae = self.assertEqual
         ul = WikiList(
             '* Lists are easy to do:\n'
             '** start every line\n'
@@ -16,12 +17,11 @@ class WikiListTest(TestCase):
             '*** deeper levels',
             pattern=r'\*')
         items = ul.items
-        self.assertEqual(items, [' Lists are easy to do:', ' with a star'])
+        ae(items, [' Lists are easy to do:', ' with a star'])
         fullitems = ul.fullitems
-        self.assertEqual(
-            fullitems, [
-                '* Lists are easy to do:\n** start every line\n',
-                '* with a star\n** more stars mean\n*** deeper levels'])
+        ae(fullitems, [
+            '* Lists are easy to do:\n** start every line\n',
+            '* with a star\n** more stars mean\n*** deeper levels'])
 
     def test_commented_list_item(self):
         """One of the list items is commented through the wikitext."""
@@ -42,6 +42,7 @@ class WikiListTest(TestCase):
         self.assertEqual(wl.items[0], '1 {{t}}')
 
     def test_subitems_for_the_first_item(self):
+        ae = self.assertEqual
         wl = WikiList(
             '# 0\n'
             '## 0.0\n'
@@ -50,15 +51,16 @@ class WikiListTest(TestCase):
             '# 2\n',
             pattern=r'\#')
         items = wl.items[0]
-        self.assertEqual(items, ' 0')
+        ae(items, ' 0')
         subitems0 = wl.sublists(0, r'\#')[0]
-        self.assertEqual(subitems0.items, [' 0.0', ' 0.1'])
+        ae(subitems0.items, [' 0.0', ' 0.1'])
         # Test to see that arguments are optional.
         sublists = wl.sublists()
-        self.assertEqual(sublists[1].fullitems, ['#* 0.0\n'])
-        self.assertEqual(sublists[0].items, [' 0.0', ' 0.1'])
+        ae(sublists[1].fullitems, ['#* 0.0\n'])
+        ae(sublists[0].items, [' 0.0', ' 0.1'])
 
     def test_subitems_for_the_second_item(self):
+        ae = self.assertEqual
         parsed = parse(
             'text\n'
             '* list item a\n'
@@ -67,10 +69,10 @@ class WikiListTest(TestCase):
             '* list item c\n'
             'text')
         wikilist = parsed.get_lists(pattern=r'\*')[0]
-        self.assertEqual(
+        ae(
             wikilist.items, [' list item a', ' list item b', ' list item c'])
         sublist = wikilist.sublists(1, r'\*')[0]
-        self.assertEqual(sublist.items, [' sub-list of b'])
+        ae(sublist.items, [' sub-list of b'])
 
     def test_mixed_definition_lists(self):
         wl = WikiList(
@@ -91,6 +93,7 @@ class WikiListTest(TestCase):
                 'back to the main list'])
 
     def test_travese_mixed_list_completely(self):
+        ae = self.assertEqual
         wl = WikiList(
             '* Or create mixed lists\n'
             '*# and nest them\n'
@@ -101,13 +104,13 @@ class WikiListTest(TestCase):
             '*#*; banana\n'
             '*#*: fruits',
             pattern=r'\*')
-        self.assertEqual(wl.items, [' Or create mixed lists'])
+        ae(wl.items, [' Or create mixed lists'])
         swl = wl.sublists(0, r'\#')[0]
-        self.assertEqual(swl.items, [' and nest them'])
+        ae(swl.items, [' and nest them'])
         sswl = swl.sublists(0, r'\*')[0]
-        self.assertEqual(sswl.items, [' like this'])
+        ae(sswl.items, [' like this'])
         ssswl = sswl.sublists(0, '[;:]')[0]
-        self.assertEqual(ssswl.items, [
+        ae(ssswl.items, [
             ' definitions',
             ' work:',
             ' apple',
@@ -115,6 +118,7 @@ class WikiListTest(TestCase):
             ' fruits'])
 
     def test_convert(self):
+        ae = self.assertEqual
         wl = WikiList(
             ':*A1\n'
             ':*#B1\n'
@@ -122,17 +126,17 @@ class WikiListTest(TestCase):
             ':*:continuing A1\n'
             ':*A2',
             pattern=r':\*')
-        self.assertEqual(wl.level, 2)
+        ae(wl.level, 2)
         wl.convert('#')
-        self.assertEqual(
+        ae(
             wl.string,
             '#A1\n'
             '##B1\n'
             '##B2\n'
             '#:continuing A1\n'
             '#A2')
-        self.assertEqual(wl.pattern, r'\#')
-        self.assertEqual(wl.level, 1)
+        ae(wl.pattern, r'\#')
+        ae(wl.level, 1)
 
     def test_cache_update(self):
         wl = WikiList('*a {{t}}', pattern=r'\*')
