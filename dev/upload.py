ef269df5361c67b383c3a44a7a7cf8b4b2d0c93d
5j9 <5j9@users.noreply.github.com>
2020-06-06 07:53:17 +0430

    build(upload.py): make sure files are deleted

--- dev/upload.py
+++ dev/upload.py
@@ -6,7 +6,9 @@ from path import Path
 
 wtp_dir = Path(__file__).parent.parent
 wtp_dir.cd()
-check_call('python setup.py sdist bdist_wheel')
-check_call('twine upload dist/*')
-for d in ('dist', 'build', 'wikitextparser.egg-info'):
-    (wtp_dir / d).rmtree()
+try:
+    check_call('python setup.py sdist bdist_wheel')
+    check_call('twine upload dist/*')
+finally:
+    for d in ('dist', 'build', 'wikitextparser.egg-info'):
+        (wtp_dir / d).rmtree()
