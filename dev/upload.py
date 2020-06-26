ec557d19df280f62f4a066546743c93bd04dd0ca
5j9 <5j9@users.noreply.github.com>
2020-06-26 12:53:23 +0430

    build(upload.py): use a tuple of args for check_call

--- dev/upload.py
+++ dev/upload.py
@@ -7,8 +7,8 @@ from path import Path
 wtp_dir = Path(__file__).parent.parent
 wtp_dir.cd()
 try:
-    check_call('python setup.py sdist bdist_wheel')
-    check_call('twine upload dist/*')
+    check_call(('python', 'setup.py', 'sdist', 'bdist_wheel'))
+    check_call(('twine', 'upload', 'dist/*'))
 finally:
     for d in ('dist', 'build', 'wikitextparser.egg-info'):
         (wtp_dir / d).rmtree()
