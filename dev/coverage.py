9f53c1dbbe3625a094d593021f0656c9fc6c4e19
5j9 <5j9@users.noreply.github.com>
2020-06-26 12:17:16 +0430

    test(.travis.yml): coverage run -m pytest

--- dev/coverage.py
+++ dev/coverage.py
@@ -6,6 +6,6 @@ from path import Path  # requires path
 
 repo = Path(__file__).parent.parent
 repo.cd()
-check_call(['coverage', 'run', 'tests'])
+check_call(['coverage', 'run', '-m', 'pytest'])
 check_call(['coverage', 'html'])
 open_new_tab(repo / 'htmlcov/index.html')
--- tests/__main__.py
+++
@@ -1,3 +0,0 @@
-from pytest import main
-
-raise SystemExit(main())
