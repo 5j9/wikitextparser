47494fe3dabf1e29855b7c93aa137e5bacc5e117
5j9 <5j9@users.noreply.github.com>
2024-04-11 07:08:37 +0330

    dev: add a profile for bold_italic

---
+++ dev/profiles/bold_italic.py
@@ -0,0 +1,19 @@
+import cProfile
+import sys
+
+from wikitextparser import parse
+
+with open('bold_italic_input.txt', encoding='utf8') as f:
+    text = f.read()
+
+profiler = cProfile.Profile()
+
+profiler.enable()
+plain_text = parse(text).plain_text()
+profiler.disable()
+
+print(plain_text)
+
+with open('bold_italic_results.txt', 'w', encoding='utf8') as f:
+    sys.stdout = f
+    profiler.print_stats(sort='tottime')
